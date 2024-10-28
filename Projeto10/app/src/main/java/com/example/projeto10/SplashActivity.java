893f8e0304dbbe713309513060497e37dbf5bde4
João <joaovictorald@gmail.com>
2024-10-28 17:07:44 -0300

    fim da aula 28/10

--- Projeto10/app/src/main/java/com/example/projeto10/SplashActivity.java
+++ Projeto10/app/src/main/java/com/example/projeto10/SplashActivity.java
@@ -1,6 +1,8 @@
 package com.example.projeto10;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +17,13 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(() ->{
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        },3000);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
